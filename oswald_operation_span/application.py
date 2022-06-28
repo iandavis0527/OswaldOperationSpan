9e595e893753c42b2b0ffa49ce1aab928b50f07c
Ian Davis <iandavis0527@gmail.com>
2022-06-28 12:08:06 -0400

    final update for changes to the cherrypy hosting scheme

--- oswald_operation_span/application.py
+++ oswald_operation_span/application.py
@@ -7,20 +7,7 @@ import cherrypy
 from cherrypy_utils import url_utils
 from cherrypy_utils.cherrypy_sqlalchemy_utils import SQLAlchemyTool, SQLAlchemyPlugin
 
-from digital_deception_emulator.backend.database import Base
-from digital_deception_emulator.backend.experiment.api import (
-    ExperimentTestApi,
-    ExperimentEventApi,
-)
-from digital_deception_emulator.backend.home.views import (
-    HomeView,
-    DashboardView,
-    HeatmapView,
-    PracticeView,
-)
-from digital_deception_emulator.backend.export.api import ExperimentExportApi
-from digital_deception_emulator.backend.login.views import LoginView
-from digital_deception_emulator.backend.configuration import (
+from oswald_operation_span.backend.configuration import (
     production_config,
     development_config,
     application_data,
@@ -42,7 +29,7 @@ def setup_server(subdomain="/digital-deception", shared_data_location=None, prod
         shared_data_location=shared_data_location,
         template_location=template_location,
         api_key_filepath=api_key_filepath,
-        produciton=production,
+        production=production,
     )
 
     cherrypy._cpconfig.environments["production"]["log.screen"] = True
@@ -62,14 +49,8 @@ def setup_server(subdomain="/digital-deception", shared_data_location=None, prod
     cherrypy.server.socket_host = "0.0.0.0"
     cherrypy.tools.oswald_operation_database = SQLAlchemyTool("oswald_operation")
 
-    cherrypy.tree.mount(HomeView(), subdomain, active_file)
-    cherrypy.tree.mount(PracticeView(), url_utils.combine_url(subdomain, "practice"), active_file)
-    cherrypy.tree.mount(LoginView(), url_utils.combine_url(subdomain, "login"), active_file)
-    cherrypy.tree.mount(HeatmapView(), url_utils.combine_url(subdomain, "heatmap"), active_file)
-    cherrypy.tree.mount(DashboardView(), url_utils.combine_url(subdomain, "dashboard"), active_file)
-    cherrypy.tree.mount(ExperimentTestApi(), url_utils.combine_url(subdomain, "api", "test"), active_file)
-    cherrypy.tree.mount(ExperimentEventApi(), url_utils.combine_url(subdomain, "api", "event"), active_file)
-    cherrypy.tree.mount(ExperimentExportApi(), url_utils.combine_url(subdomain, "api", "export"), active_file)
+    # TODO: Mount urls here.
+    # cherrypy.tree.mount(HomeView(), subdomain, active_file)
 
     mysql_filepath = str(server_directory.joinpath("mysql.credentials").resolve())
 
@@ -84,14 +65,15 @@ def setup_server(subdomain="/digital-deception", shared_data_location=None, prod
         database_filepath = str(pathlib.Path(shared_data_location, "digital_deception.db").resolve())
         connection_string = "sqlite:///" + database_filepath
 
-    SQLAlchemyPlugin(
-        "oswald_operation",
-        cherrypy.engine,
-        Base,
-        connection_string,
-        echo=False,
-        pool_recycle=20000,
-    )
+    # TODO: Bring back once database schema has been defined
+    # SQLAlchemyPlugin(
+    #     "oswald_operation",
+    #     cherrypy.engine,
+    #     Base,
+    #     connection_string,
+    #     echo=False,
+    #     pool_recycle=20000,
+    # )
 
     cherrypy.log("Publishing db create for digital_deception")
     cherrypy.engine.publish("oswald_operation.db.create")
